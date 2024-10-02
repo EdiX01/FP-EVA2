94f4b24b5c9429ef9240418be2a34a976a973a91
EdiX01 <L24550375@chihuahua2.tecnm.mx>
2024-10-02 17:48:22 -0600

    Add files via upload

---
+++ EVA2_1_CICLOS/src/eva2_1_ciclos/EVA2_1_CICLOS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1_ciclos;
+
+/**
+ *
+ * @author EdiX
+ */
+public class EVA2_1_CICLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 1; i <= 10; i++){
+            System.out.print(i +" - ");
+        }
+        for(int x = 10; x <= 1; x++){
+            System.out.print(x +" - ");
+        }
+    }
+    
+}
