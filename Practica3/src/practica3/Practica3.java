2e221f1e49cdf9eee04aafd6696a0a6f569f85dc
EdiX01 <L24550375@chihuahua2.tecnm.mx>
2024-10-08 17:55:26 -0600

    Add files via upload

---
+++ Practica3/src/practica3/Practica3.java
@@ -0,0 +1,22 @@
+
+package practica3;
+import java.util.Scanner;
+/**
+ *
+ * @author invitado
+ */
+public class Practica3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner(System.in);
+        String Cad;
+        System.out.println("Introduce una cadena de texto: ");
+        Cad = captu.nextLine();
+        System.out.println();
+    }
+    
+}
