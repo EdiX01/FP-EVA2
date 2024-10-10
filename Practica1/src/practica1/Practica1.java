d3b88b68394253fd6649ab4d8fef5206c70efedd
EdiX01 <L24550375@chihuahua2.tecnm.mx>
2024-10-10 17:39:53 -0600

    Add files via upload

---
+++ Practica1/src/practica1/Practica1.java
@@ -0,0 +1,28 @@
+package practica1;
+import java.util.Scanner;
+/**
+ *
+ * @author invitado
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner(System.in);
+        int Num;
+        String Cad;
+        System.out.println("Introduce una cadena de texto: ");
+        Cad = captu.nextLine();
+        System.out.println("Introduce el numero de veces que se va a repetir");
+        Num = captu.nextInt();
+        
+        for(int i = 1 ; i <= Num; i++){
+            System.out.println(Cad);
+        }
+        
+    }
+    
+}
