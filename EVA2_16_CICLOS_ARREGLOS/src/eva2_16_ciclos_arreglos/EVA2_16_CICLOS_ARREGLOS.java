08339b675922888e3fd890045a85f84684b86bd1
EdiX01 <L24550375@chihuahua2.tecnm.mx>
2024-10-21 17:48:44 -0600

    Add files via upload

---
+++ EVA2_16_CICLOS_ARREGLOS/src/eva2_16_ciclos_arreglos/EVA2_16_CICLOS_ARREGLOS.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_16_ciclos_arreglos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_16_CICLOS_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+        int[] datos, x, y, z;
+        int val1, val2, arreglo[], val3;
+        
+        arreglo = new int[10];
+        
+        for (int i = 0; i < 10; i++){
+            arreglo[i] =(int)(Math.random() * 100);
+        }
+                for (int i = 0; i < 10; i++){
+                    System.out.println(arreglo[i]);
+                }
+    }
+    
+}
